3f23769937a4a21e1c4650517383f4e47366bfdb
Roelec <gabrielruas10@gmail.com>
2023-11-24 21:25:56 -0300

    até aula 6

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,10 +1,14 @@
+import random
+
+
 print("*********************************")
 print("Bem-vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
+numero_secreto = random.randrange(1,101)
 total_de_tentativas = 3
 
+print(numero_secreto)
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -19,7 +23,7 @@ for rodada in range(1, total_de_tentativas + 1):
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
     menor   = chute < numero_secreto
-ujfejifd
+
     if(acertou):
         print("Você acertou!")
         break
